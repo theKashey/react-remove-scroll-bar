7f67e99fed27ba44c1a978a5efaa473a1cddb2fe
Anton Korzunov <thekashey@gmail.com>
2022-04-17 16:12:43 +1000

    update infrastructure

--- src/component.tsx
+++ src/component.tsx
@@ -1,7 +1,8 @@
 import * as React from 'react';
-import {styleSingleton} from 'react-style-singleton';
-import {GapMode, GapOffset, getGapWidth} from './utils';
-import {fullWidthClassName, zeroRightClassName, noScrollbarsClassName, removedBarSizeVariable} from "./constants";
+import { styleSingleton } from 'react-style-singleton';
+
+import { fullWidthClassName, zeroRightClassName, noScrollbarsClassName, removedBarSizeVariable } from './constants';
+import { GapMode, GapOffset, getGapWidth } from './utils';
 
 export interface BodyScroll {
   noRelative?: boolean;
@@ -14,17 +15,22 @@ const Style = styleSingleton();
 // important tip - once we measure scrollBar width and remove them
 // we could not repeat this operation
 // thus we are using style-singleton - only the first "yet correct" style will be applied.
-const getStyles = ({left, top, right, gap}: GapOffset, allowRelative: boolean, gapMode: GapMode = 'margin', important: string) => `
+const getStyles = (
+  { left, top, right, gap }: GapOffset,
+  allowRelative: boolean,
+  gapMode: GapMode = 'margin',
+  important: string
+) => `
   .${noScrollbarsClassName} {
    overflow: hidden ${important};
    padding-right: ${gap}px ${important};
   }
   body {
     overflow: hidden ${important};
-    ${
-  [
-    allowRelative && `position: relative ${important};`,
-    gapMode === 'margin' && `
+    ${[
+      allowRelative && `position: relative ${important};`,
+      gapMode === 'margin' &&
+        `
     padding-left: ${left}px;
     padding-top: ${top}px;
     padding-right: ${right}px;
@@ -32,9 +38,10 @@ const getStyles = ({left, top, right, gap}: GapOffset, allowRelative: boolean, g
     margin-top:0;
     margin-right: ${gap}px ${important};
     `,
-    gapMode === 'padding' && `padding-right: ${gap}px ${important};`,
-  ].filter(Boolean).join('')
-  }
+      gapMode === 'padding' && `padding-right: ${gap}px ${important};`,
+    ]
+      .filter(Boolean)
+      .join('')}
   }
   
   .${zeroRightClassName} {
@@ -58,14 +65,17 @@ const getStyles = ({left, top, right, gap}: GapOffset, allowRelative: boolean, g
   }
 `;
 
+/**
+ * Removes page scrollbar and blocks page scroll when mounted
+ */
 export const RemoveScrollBar: React.FC<BodyScroll> = (props) => {
-  const [gap, setGap] = React.useState(getGapWidth(props.gapMode))
+  const [gap, setGap] = React.useState(getGapWidth(props.gapMode));
 
   React.useEffect(() => {
     setGap(getGapWidth(props.gapMode));
   }, [props.gapMode]);
 
-  const {noRelative, noImportant, gapMode = 'margin'} = props;
+  const { noRelative, noImportant, gapMode = 'margin' } = props;
 
-  return <Style styles={getStyles(gap, !noRelative, gapMode, !noImportant ? "!important" : '')}/>;
+  return <Style styles={getStyles(gap, !noRelative, gapMode, !noImportant ? '!important' : '')} />;
 };
