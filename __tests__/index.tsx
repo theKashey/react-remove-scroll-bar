2eae7247eeb133c229c7b4617a8cf61570b5af6d
Anton Korzunov <thekashey@gmail.com>
2022-05-01 20:03:22 +1000

    suppress default test

--- __tests__/index.tsx
+++ __tests__/index.tsx
@@ -1,13 +1,4 @@
-import * as React from 'react';
-import {shallow} from 'enzyme';
-
 describe('Specs', () => {
-  const setup = () => {
-    return {
-      
-    };
-  };
-  
   it('Foo', () => {
     expect(1).toEqual(1);
   });
