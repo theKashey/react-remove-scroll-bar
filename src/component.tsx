0c7b60c0f993615c5d7ccc173e4e6ebd35e693e1
Mark Chambers <mark.chambers@sharesies.co.nz>
2022-05-11 16:44:00 +1200

    feat: use css vars with fallbacks

--- src/component.tsx
+++ src/component.tsx
@@ -5,9 +5,9 @@ import { fullWidthClassName, zeroRightClassName, noScrollbarsClassName, removedB
 import { GapMode, GapOffset, getGapWidth } from './utils';
 
 export interface BodyScroll {
-  noRelative?: boolean;
-  noImportant?: boolean;
-  gapMode?: GapMode;
+    noRelative?: boolean;
+    noImportant?: boolean;
+    gapMode?: GapMode;
 }
 
 const Style = styleSingleton();
@@ -16,10 +16,10 @@ const Style = styleSingleton();
 // we could not repeat this operation
 // thus we are using style-singleton - only the first "yet correct" style will be applied.
 const getStyles = (
-  { left, top, right, gap }: GapOffset,
-  allowRelative: boolean,
-  gapMode: GapMode = 'margin',
-  important: string
+    { left, top, right, gap }: GapOffset,
+    allowRelative: boolean,
+    gapMode: GapMode = 'margin',
+    important: string
 ) => `
   .${noScrollbarsClassName} {
    overflow: hidden ${important};
@@ -29,28 +29,35 @@ const getStyles = (
     overflow: hidden ${important};
     overscroll-behavior: contain;
     ${[
-      allowRelative && `position: relative ${important};`,
-      gapMode === 'margin' &&
-        `
+    allowRelative && `position: relative ${important};`,
+    gapMode === 'margin' &&
+    `
     padding-left: ${left}px;
     padding-top: ${top}px;
     padding-right: ${right}px;
-    margin-left:0;
-    margin-top:0;
+    margin-left: 0;
+    margin-top: 0;
     margin-right: ${gap}px ${important};
+    margin-right: var(${removedBarSizeVariable}, 0) ${important};
     `,
-      gapMode === 'padding' && `padding-right: ${gap}px ${important};`,
-    ]
-      .filter(Boolean)
-      .join('')}
+    gapMode === 'padding' && (
+        `
+        padding-right: ${gap}px ${important};
+        padding-right: var(${removedBarSizeVariable}, 0) ${important};`
+    ),
+]
+    .filter(Boolean)
+    .join('')}
   }
   
   .${zeroRightClassName} {
     right: ${gap}px ${important};
+    right: var(${removedBarSizeVariable}, 0) ${important};
   }
   
   .${fullWidthClassName} {
     margin-right: ${gap}px ${important};
+    margin-right: var(${removedBarSizeVariable}, 0) ${important};
   }
   
   .${zeroRightClassName} .${zeroRightClassName} {
@@ -70,13 +77,13 @@ const getStyles = (
  * Removes page scrollbar and blocks page scroll when mounted
  */
 export const RemoveScrollBar: React.FC<BodyScroll> = (props) => {
-  const [gap, setGap] = React.useState(getGapWidth(props.gapMode));
+    const [gap, setGap] = React.useState(getGapWidth(props.gapMode));
 
-  React.useEffect(() => {
-    setGap(getGapWidth(props.gapMode));
-  }, [props.gapMode]);
+    React.useEffect(() => {
+        setGap(getGapWidth(props.gapMode));
+    }, [props.gapMode]);
 
-  const { noRelative, noImportant, gapMode = 'margin' } = props;
+    const { noRelative, noImportant, gapMode = 'margin' } = props;
 
-  return <Style styles={getStyles(gap, !noRelative, gapMode, !noImportant ? '!important' : '')} />;
+    return <Style styles={getStyles(gap, !noRelative, gapMode, !noImportant ? '!important' : '')} />;
 };
